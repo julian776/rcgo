d3bec3b3d4de914467f8d94c58c36f0e7f90b3f1
Julian Alvarez <juliancamiloalvarez77@gmail.com>
2024-01-12 23:35:22 -0500

    Add example e2e

---
+++ examples/e2e/listener.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/julian776/rcgo"
+)
+
+func main() {
+	lname := "testListener"
+
+	configs := rcgo.ListenerConfigs{
+		Url:             "amqp://user:password@localhost",
+		DelayOnReject:   time.Second * 5,
+		AckIfNoHandlers: true,
+		Timezone:        time.UTC,
+		LogLevel:        "info",
+	}
+
+	l := rcgo.NewListener(
+		configs,
+		lname,
+	)
+
+	l.AddCommandHandler(
+		"testListener.print",
+		func(ctx context.Context, c *rcgo.Cmd) error {
+			m := c.Data.(map[string]interface{})
+
+			fmt.Printf("msg received %+v\n", m)
+
+			return nil
+		},
+	)
+
+	l.AddEventHandler(
+		"orderPlaced",
+		func(ctx context.Context, e *rcgo.Event) error {
+			m := e.Data.(map[string]interface{})
+
+			fmt.Printf("order received %+v\n", m)
+
+			return nil
+		},
+	)
+
+	l.AddQueryHandler(
+		"testListener.employees",
+		func(ctx context.Context, q *rcgo.Query) (interface{}, error) {
+			req := q.Data.(map[string]interface{})
+
+			id := req["id"]
+
+			fmt.Printf("id received %s\n", id)
+
+			// Fetch data
+
+			return map[string]interface{}{
+				"id":   id,
+				"name": "julian",
+			}, nil
+		},
+	)
+
+	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+
+	go l.Listen(ctx)
+
+	<-ctx.Done()
+
+	l.Stop()
+}
---
+++ examples/e2e/publisher.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/julian776/rcgo"
+)
+
+func main() {
+	pname := "testPublisher"
+	lname := "testListener"
+
+	configs := &rcgo.PublisherConfigs{
+		Url:          "amqp://user:password@localhost",
+		ReplyTimeout: time.Second * 15,
+	}
+
+	p := rcgo.NewPublisher(
+		configs,
+		pname,
+	)
+
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*60)
+
+	data := map[string]string{
+		"app":       "reactive-commons-go",
+		"shortname": "rcgo",
+		"name":      "julian776",
+	}
+	p.SendCmd(ctx, lname, "testListener.print", data)
+
+	time.Sleep(time.Second * 5)
+
+	edata := map[string]string{
+		"id":      "US-NY-5434789",
+		"product": "rcgo",
+		"price":   "776",
+	}
+	p.PublishEvent(ctx, "orderPlaced", edata)
+
+	time.Sleep(time.Second * 5)
+
+	qdata := map[string]string{
+		"id": "776",
+	}
+
+	var res interface{}
+	err := p.RequestReply(ctx, lname, "testListener.employees", qdata, &res)
+	if err != nil {
+		fmt.Errorf("err in RequestReply %s", err.Error())
+	}
+
+	fmt.Printf("reply %+v", res)
+
+	time.Sleep(time.Second * 5)
+}
