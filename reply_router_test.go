907d35e8ce04c183efaaa27783b504e01a36b39a
Julian Alvarez <juliancamiloalvarez77@gmail.com>
2024-01-19 20:22:05 -0500

    Add ReplyRouterTestSuite

---
+++ reply_router_test.go
@@ -0,0 +1,42 @@
+package rcgo
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+)
+
+func TestReplyRouterTestSuite(t *testing.T) {
+	suite.Run(t, new(ReplyRouterTestSuite))
+}
+
+type ReplyRouterTestSuite struct {
+	suite.Suite
+	*replyRouter
+}
+
+func (s *ReplyRouterTestSuite) SetupSuite() {
+	s.replyRouter = newReplyRouter("test", time.Second*15)
+}
+
+func (s *ReplyRouterTestSuite) Test_addReplyToListen() {
+	query := "query"
+	corrId := "123"
+	timeout := time.Millisecond * 100
+
+	s.timeout = timeout
+
+	start := time.Now()
+
+	replyCh := s.replyRouter.addReplyToListen(query, corrId)
+
+	replyStr, ok := s.replyRouter.repliesMap[corrId]
+	s.True(ok)
+	s.Equal(query, replyStr.query)
+
+	reply := <-replyCh
+
+	s.WithinDuration(start.Add(timeout), time.Now(), time.Millisecond*10)
+	s.EqualError(reply.err, "timeout while waiting for a reply")
+}
